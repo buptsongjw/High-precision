d1f2c7e100dd086fca53bdc7a4b8d39e82f65c68
buptsongjw <1484832578@qq.com>
2016-10-10 21:23:56 +0800

    Create high-precision-base.h

---
+++ src/high-precision-base.h
@@ -0,0 +1,20 @@
+//high-precision
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+class hpnumber
+{
+	private:
+		string value;
+	public:
+		hpnumber(){}
+		hpnumber(string a)
+		{
+			value=a;
+		}
+		hpnumber add(hpnumber a,hpnumber b);
+		hpnumber sub(hpnumber a,hpnumber b);
+		hpnumber muti(hpnumber a,hpnumber b);
+		hpnumber dvi(hpnumber a,hpnumber b);	
+};
